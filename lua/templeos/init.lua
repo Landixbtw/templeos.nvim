4189bd7cb59e75f8f260ebe06414074681947c59
Landixbtw <68870626+Landixbtw@users.noreply.github.com>
2025-03-15 16:19:06 +0100

    Update init.lua

--- lua/templeos/init.lua
+++ lua/templeos/init.lua
@@ -1,18 +1,17 @@
 local M = {}
 local theme = require('templeos.theme')
-
 M.setup = function()
   vim.cmd('hi clear')
-
   vim.o.background = 'light'
   if vim.fn.exists('syntax_on') then
     vim.cmd('syntax reset')
   end
-
   vim.o.termguicolors = true
   vim.g.colors_name = 'templeos'
-
+  
+  -- Add this line to set the cursor appearance
+  vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20"
+  
   theme.set_highlights()
 end
-
 return M
