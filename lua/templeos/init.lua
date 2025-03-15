f8fc3686bcdca439a8bc0c33f7e3b363210ad122
Landixbtw <68870626+Landixbtw@users.noreply.github.com>
2025-03-15 16:26:24 +0100

    Update init.lua

--- lua/templeos/init.lua
+++ lua/templeos/init.lua
@@ -8,9 +8,19 @@ M.setup = function()
   end
   vim.o.termguicolors = true
   vim.g.colors_name = 'templeos'
-  
+  -- Add these lines after your colorscheme is loaded
+vim.cmd([[
+  highlight Cursor guifg=white guibg=black
+  highlight iCursor guifg=white guibg=black
+  highlight vCursor guifg=white guibg=black
+  highlight lCursor guifg=white guibg=black
+  highlight CursorIM guifg=white guibg=black
+]])
+
+-- Set guicursor to use these highlight groups
+  vim.opt.guicursor = "n-v-c-sm:block-Cursor,i-ci-ve:ver25-iCursor,r-cr-o:hor20-Cursor"
   -- Add this line to set the cursor appearance
-  vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20"
+  -- vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20"
   
   theme.set_highlights()
 end
